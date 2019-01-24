004f7b5dc5fd1c1b339dbec8fe9d69366fb76389
LokeshRs <lokesh.rs12@gmail.com>
2019-01-24 14:33:11 +0530

    first commit

---
+++ app/src/main/java/adefault/loginscreen/MainActivity.java
@@ -0,0 +1,28 @@
+package adefault.loginscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button n = (Button)findViewById(R.id.button_login);
+        n.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this , LoginActivity.class);
+                startActivity(myIntent);
+
+
+            }
+        });
+    }
+}
