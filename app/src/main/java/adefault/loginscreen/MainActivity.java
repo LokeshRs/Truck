74f7a30e853864f754b7e06e4185522d738e2fa0
LokeshRs <lokesh.rs12@gmail.com>
2019-01-25 21:19:58 +0530

    dialogue box

--- app/src/main/java/adefault/loginscreen/MainActivity.java
+++ app/src/main/java/adefault/loginscreen/MainActivity.java
@@ -1,12 +1,21 @@
 package adefault.loginscreen;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.widget.CircularProgressDrawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import static adefault.loginscreen.R.id.button_login;
+import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,22 +24,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button n = (Button)findViewById(R.id.button_login);
+        Button n = (Button)findViewById(button_login);
+
+
+
+
         n.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText email = (EditText) findViewById(R.id.email_id);
                 EditText pwd =(EditText) findViewById(R.id.pass_id);
+                                Thread mythread = new Thread();
+
                 if(email.getText().toString().equals("abc@123") && pwd.getText().toString().equals("1234")) {
+
                     Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
                     startActivity(myIntent);
                 }
                 else
                 {
-                    Intent myIntent = new Intent(MainActivity.this, loginFailed.class);
-                    startActivity(myIntent);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle(R.string.login_fail);
+                    builder.setMessage(R.string.msg);
+                    builder.show();
+
+
+
                 }
             }
         });
-    }
+
+        }
 }
